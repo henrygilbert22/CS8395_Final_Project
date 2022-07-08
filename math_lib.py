b349890a23c498685d682055464366f259114a89
henrygilbert22 <hg19b@my.fsu.edu>
2022-07-07 23:12:38 -0500

    Added softmax activation function

--- math_lib.py
+++ math_lib.py
@@ -1,5 +1,6 @@
+import numpy as np
 
-def ReLU(x: list) -> list:
+def ReLU(X: list) -> list:
     """ ReLU activation function 
     
     Args:
@@ -9,4 +10,18 @@ def ReLU(x: list) -> list:
         list of numbers
     """
 
-    return x * (x > 0)
+    return X * (X > 0)
+
+def softmax_activation(X: list) -> list:
+    """ Applies Softmax activation function to
+    list of floats 
+    
+    Args:
+        x: list of numbers
+    
+    Returns:
+        list of numbers
+    """
+
+    probabilities = [np.exp(x) / np.sum(np.exp(x)) for x in X]
+    return np.array(probabilities)
