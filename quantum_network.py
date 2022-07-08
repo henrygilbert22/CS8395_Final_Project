238ce63e2750bf15a20526df27b59b58e1a3c47c
henrygilbert22 <hg19b@my.fsu.edu>
2022-07-07 23:31:44 -0500

    Added full backward_pass

--- quantum_network.py
+++ quantum_network.py
@@ -76,10 +76,50 @@ class QuantumNetwork():
 
 		W1, b1 = self.params['W1'], self.params['b1']
 		W2, b2 = self.params['W2'], self.params['b2']
-		N, D = X.shape
 
 		layer_1_output = np.dot(X, W1) + b1
 		layer_1_output = math_lib.ReLU(layer_1_output)
 		layer_2_output = np.dot(layer_1_output, W2) + b2
 		
-		return layer_2_output
+		return layer_2_output, layer_1_output
+
+	def backward_pass(self, layer_2_output: np.array, layer_1_output: np.array, 
+		X: np.array, y: list, loss: float, learning_rate: float = 0.01, 
+		regularization: float = 0.0) -> None:
+		""" Forward pass of the network.
+		
+		Args:
+			X: list of input data
+			y: list of labels
+			learning_rate: float
+			regularization: float
+		
+		Returns:
+			None
+		"""
+
+		W1, b1 = self.params['W1'], self.params['b1']
+		W2, b2 = self.params['W2'], self.params['b2']
+		N, _ = X.shape
+
+		probabilities = self.softmax(layer_2_output)
+
+		probabilities[(range(N), y)] -= 1
+		dZ2 = probabilities / N
+
+		dW2 = np.dot(layer_1_output.T, dZ2)
+		db2 = np.ones((1, layer_1_output.shape[0])).dot(dZ2)
+
+		dA1 = dZ2.dot(W2.T)
+		dZ1 = dA1.copy()
+		dZ1[layer_1_output <= 0] = 0
+		dW1 = X.T.dot(dZ1)
+		db1 = np.ones((1, N)).dot(dZ1)
+
+		dW1 += 2 * regularization * W1
+		dW2 += 2 * regularization * W2
+
+		self.parameters['W1'] -= learning_rate * dW1
+		self.parameters['b1'] -= learning_rate * db1[0]
+		self.parameters['W2'] -= learning_rate * dW2
+		self.parameters['b2'] -= learning_rate * db2[0]
