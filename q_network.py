a78ed6444e5fa7799c915d2eecb0ffaafee41e05
henrygilbert22 <hg19b@my.fsu.edu>
2022-07-22 17:53:37 +0000

    update

--- q_network.py
+++ q_network.py
@@ -13,9 +13,11 @@ class QNetwork(SamplingNeuralNetwork):
     _weight_params: List[Parameter]
     _input_gradients: bool
     
+    _gradient_circuit: OperatorBase
     _circuit: QuantumCircuit
     
     _original_output_shape: Union[int, Tuple[int, ...]]
+    _output_shape: Union[int, Tuple[int, ...]]
     _interpret: Callable[[int], Union[int, Tuple[int, ...]]]
     _quantum_instance: QuantumInstance
     
@@ -26,7 +28,6 @@ class QNetwork(SamplingNeuralNetwork):
         weight_params: List[Parameter],
         interpret: Callable[[int], Union[int, Tuple[int, ...]]],
         output_shape: Union[int, Tuple[int, ...]],
-        gradient: Gradient,
         quantum_instance: QuantumInstance,
         input_gradients: bool = False,
         ) -> None:
@@ -39,10 +40,11 @@ class QNetwork(SamplingNeuralNetwork):
         self._circuit_transpiled = False
        
         self._original_output_shape = output_shape
-
+        self._output_shape = output_shape
+        
         self._interpret = interpret
         self._quantum_instance = quantum_instance
-        self._set_quantum_instance()
+        
 
         super().__init__(
             len(self._input_params),
@@ -54,36 +56,14 @@ class QNetwork(SamplingNeuralNetwork):
         )
 
         self._original_circuit = circuit
-        self._gradient = gradient or Gradient()
+        self._gradient = Gradient()
+        
+        grad_circuit = self._original_circuit.copy()
+        grad_circuit.remove_final_measurements()
+        
+        self._gradient_circuit = self._gradient.convert(
+            StateFn(grad_circuit), 
+            self._input_params + self._weight_params)
+        
 
-        self._construct_gradient_circuit()
-    
-    def _set_quantum_instance(self) -> None:
-       
-
-        if self._quantum_instance is not None:
-            # add measurements in case none are given
-            if self._quantum_instance.is_statevector:
-                if len(self._circuit.clbits) > 0:
-                    self._circuit.remove_final_measurements()
-            elif len(self._circuit.clbits) == 0:
-                self._circuit.measure_all()
-
-            # set interpret and compute output shape
-            self.set_interpret(interpret, output_shape)
-
-            # prepare sampler
-            self._sampler = CircuitSampler(self._quantum_instance, param_qobj=False, caching="all")
-
-            # transpile the QNN circuit
-            try:
-                self._circuit = self._quantum_instance.transpile(
-                    self._circuit, pass_manager=self._quantum_instance.unbound_pass_manager
-                )[0]
-                self._circuit_transpiled = True
-            except QiskitError:
-                # likely it is caused by RawFeatureVector, we just ignore this error and
-                # transpile circuits when it is required.
-                self._circuit_transpiled = False
-        else:
-            self._output_shape = output_shape
+   
